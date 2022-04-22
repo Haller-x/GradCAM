cabcc8c35383c1e36aa1dfc349763d1959322828
Haller-x <geyoneda@hotmail.com>
2022-04-22 14:28:14 -0300

    fixing readme and improving imports

--- README.md
+++ README.md
@@ -15,11 +15,15 @@ Building the container
 Acessing the container (jupyter notebook)
 `docker run --gpus all -v "$(pwd)":/code -p 8888:8888 env_name`
 
+## Limitations:
+The model should preferably be built using tensorflow:nightly-gpu-jupyter to avoid compatibility issues.
+
+
 Using the module
 ```
 from gradcam import GradCAM
 #loading model and image size
-gd = Gradcam('model.h5',size=(300,300))
+gd = GradCAM('model.h5',size=(300,300))
 #display
-gd.pure_display(img_path='test.jpg')
+gd.display(img_path='test.jpg')
 ``` 
--- gradcam.py
+++ gradcam.py
@@ -1,16 +1,17 @@
 #importações libs
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import pandas as pd
 import numpy as np
+
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.models import load_model
 
 
-class GradCam:
+class GradCAM:
     
     def __init__(self, model:str, size:tuple):
         """
@@ -21,7 +22,7 @@ class GradCam:
         self.model = load_model(model)
         self.size = size
         for layer in self.model.layers:
-            if isinstance(layer, layers.Conv2D):
+            if isinstance(layer, Conv2D):
                 self.conv_layer = layer.name
   
     def display(self, img_path:str, pred_index=None,alpha=0.4):
@@ -32,11 +33,11 @@ class GradCam:
             pred_index (int, optional): Index of the desired activation class. Defaults to None.
             alpha (float, optional): transparency index. Defaults to 0.4.
         """
-        grad_model = tf.keras.models.Model(
+        grad_model = Model(
             [self.model.inputs], [self.model.get_layer(self.conv_layer).output, self.model.output]
         )
-        img = keras.preprocessing.image.load_img(img_path, target_size=self.size)
-        array = keras.preprocessing.image.img_to_array(img)
+        img = load_img(img_path, target_size=self.size)
+        array = img_to_array(img)
         array = preprocess_input(np.expand_dims(array, axis=0))
         with tf.GradientTape() as tape:
             last_conv_layer_output, preds = grad_model(array)
@@ -51,17 +52,17 @@ class GradCam:
         heatmap = tf.squeeze(heatmap)
         heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
         heatmap = heatmap.numpy()
-        img = keras.preprocessing.image.load_img(img_path)
-        img = keras.preprocessing.image.img_to_array(img)
+        img = load_img(img_path)
+        img = img_to_array(img)
         heatmap = np.uint8(255 * heatmap)
         jet = cm.get_cmap("jet")
         jet_colors = jet(np.arange(256))[:, :3]
         jet_heatmap = jet_colors[heatmap]
-        jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+        jet_heatmap = array_to_img(jet_heatmap)
         jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
-        jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+        jet_heatmap = img_to_array(jet_heatmap)
         superimposed_img = jet_heatmap * alpha + img
-        superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+        superimposed_img = array_to_img(superimposed_img)
 
         imgplot = plt.imshow(superimposed_img)
         plt.show()
